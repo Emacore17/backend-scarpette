bafe1267b1efecd9c2ffcbd1d2e6ff72f9bcbd77
Emanuele Anzaldi <emanuele.anzaldi@outlook.it>
2024-01-03 14:29:16 +0100

    add pippo

--- src/scarpetta/scarpetta.service.ts
+++ src/scarpetta/scarpetta.service.ts
@@ -21,6 +21,10 @@ export class ScarpettaService {
     return 'all scarpette';
   }
 
+  pippo() {
+    return 'pippo';
+  }
+
   async createScarpetta(createScarpettaDto: any) {
     return await this.scarpettaRepo.manager.transaction(
       async (entityManager) => {
